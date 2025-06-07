4e7faca743a2e30240e5764a6aad7fcba5f90452
vincent-mugendi <vincentmwathi5@gmail.com>
2025-06-07 23:53:13 +0300

    feat: Adding SkipCard.tsx to omponent to display each skip's detailed card.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,10 @@
+import SkipHire from "./pages/SkipHire";
+
 function App() {
   return (
-    <div className="flex items-center justify-center h-screen bg-blue-100">
-      <h1 className="text-3xl font-bold text-blue-700">Hello Vite + React + Tailwind!</h1>
-    </div>
+    <>
+      <SkipHire />
+    </>
   );
 }
 
